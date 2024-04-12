1670e8b968e271de76d75fd36292438e16f0a151
Matheus Velloso <matheus.vn@outlook.com>
2024-04-12 11:19:21 -0300

    Added [config]: DB and logs path

--- config/config.py
+++ config/config.py
@@ -2,4 +2,6 @@ from pathlib import Path
 
 HOST = 'ws://localhost:4848/app/'
 APPS_PATH = Path('G:/Shared Drives/Tax_Technology/Qlik/eSocial/Apps/E - Extract/')
+DB_PATH = Path('C:/Workspace/QlikSenseReloader/database/sql')
 CPU_CORES = 4
+LOGS_PATH = Path('C:/Users/Clark/Documents/Qlik/Sense/Log/Script/')
